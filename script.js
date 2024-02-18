28e3391920c6d13f328488a771d18cf11d13e93a
manik-kyau <you@example.commaniksarkar.kyau.cse>
2024-02-18 11:09:53 -0800

    input field hidden and discount calculation

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ for(let i = 0;  i <allButton.length; i++){
     const bttn = allButton[i];
     bttn.addEventListener('click',function(event){
         count = count + 1;
+        // enable Apply button
+        if(count >= 4){
+            document.getElementById('apply-button').disabled = false;
+        }
+
 
         // set backgrounf color of seet
         const bgColor = event.target;
@@ -11,7 +16,6 @@ for(let i = 0;  i <allButton.length; i++){
         bgColor.style.color = 'white';
         event.target.setAttribute('disabled', true)
         
-
         // ticket title container
         const title = bttn.querySelector('h3').innerText;
         const titleContainer = document.getElementById('title-containet');
@@ -27,14 +31,15 @@ for(let i = 0;  i <allButton.length; i++){
         priceContainer('price-container');
         setInnerText('seat-count', count);
         setInnerText('seat-left', seatCount);
+        
+
     })
 }
 // Scroll option
 function scrollParibahan(){
     const scrollSection = document.getElementById('scroll');
     scrollSection.scrollIntoView({behavior:'smooth'});
-
-}
+};
 
 // economoy container 
 function classContainer(id){
@@ -42,7 +47,7 @@ function classContainer(id){
     const p = document.createElement('p');
     p.innerText = 'Economoy';
     titleContainer.appendChild(p);
-}
+};
 
 // price container 
 function priceContainer(id){
@@ -50,17 +55,43 @@ function priceContainer(id){
     const p = document.createElement('p');
     p.innerText = '550'
     priceContainer.appendChild(p);
-    const ticketPrice = parseInt(document.getElementById('price-container').childNodes[1].innerText);
+    const ticketPrice = parseFloat(document.getElementById('price-container').childNodes[1].innerText);
     // sum of total ticket price
     const totalPrice = document.getElementById('total');
-    const price = parseInt(document.getElementById('total').innerText);
+    const price = parseFloat(document.getElementById('total').innerText);
     const sum = ticketPrice + price;
     totalPrice.innerText = sum;
 
     // Grand Total Price
     const grandTotal = document.getElementById('grandtotal');
     grandTotal.innerText = sum;
-}
+};
+// work of input field and coupon
+function inputCouponCode(){
+
+    hideSectionById('hidden-section');
+    
+    const inputCoupon = document.getElementById('input-field').value;
+    const grandTotalCost = parseInt(document.getElementById('grandtotal').innerText);
+    if(inputCoupon === 'NEW15'){
+        const discount = grandTotalCost - (grandTotalCost * 0.15);
+        setInnerText('grandtotal', discount);
+    }
+
+    else if(inputCoupon === 'Couple 20'){
+        const discount = grandTotalCost - (grandTotalCost * 0.2);
+        setInnerText('grandtotal', discount);
+    }
+    else{
+        alert('Invalid Coupon Code');
+    }
+    
+};
+function hideSectionById(sectionId){
+    const hideSection = document.getElementById(sectionId);
+    const hide = hideSection.classList.add('hidden');
+};
+//  set inner text
 function setInnerText(id, value){
     document.getElementById(id).innerText = value;
 };
